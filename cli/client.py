33807d60adf308d8b2d5a8e2fa67dbe97e3dd7f3
Teodor Garzdin <tsg@hugelawn.com>
2016-10-17 22:46:52 +0200

    Remove whitespace.

--- cli/client.py
+++ cli/client.py
@@ -288,7 +288,7 @@ class Client(object):
         Get mediator information
         # Parameters
             * mediator_id [int/string] - The id of the mediator
-            * from_date [string] (оptional) -  The date against to cross check
+            * from_date [string] (оptional) - The date against to cross check
         """
         if from_date:
             if not match(r"[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$", from_date):
