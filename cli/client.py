fdcb208f5b3d59ea57e1ef7bf1dc5c561a16f77d
Teodor Garzdin <tsg@hugelawn.com>
2016-10-17 17:31:32 +0200

    Create custom exceptions and add client class.

---
+++ cli/client.py
@@ -0,0 +1,108 @@
+import http
+import exceptions
+
+ECONT_DEMO_URL = "http://demo.econt.com/e-econt/xml_parcel_import.php"
+ECONT_DEMO_SERVICE_URL = "http://demo.econt.com/e-econt/xml_service_tool.php"
+ECONT_URL = "http://www.econt.com/e-econt/xml_parcel_import.php"
+ECONT_SERVICE_URL = "http://www.econt.com/e-econt/xml_service_tool.php"
+
+REQUEST_SHIPMENTS = 0
+REQUEST_CITIES_ZONES = 1
+REQUEST_CITIES = 2
+REQUEST_CITIES_REGIONS = 3
+REQUEST_CITIES_STREETS = 4
+REQUEST_CITIES_QUARTERS = 5
+REQUEST_OFFICES = 6
+REQUEST_POST_BOXES = 7
+REQUEST_TARIFF_COURIER = 8
+REQUEST_TARIFF_POST = 9
+REQUEST_PROFILE = 10
+REQUEST_REGISTRATION_REQUEST = 11
+REQUEST_COUNTRIES = 12
+REQUEST_CANCEL_SHIPMENTS = 13
+REQUEST_DELIVERY_DAYS = 14
+REQUEST_ACCOUNT_ROLES = 15
+REQUEST_CLIENT_INFO = 16
+REQUEST_ACCESS_CLIENTS = 17
+REQUEST_CHECK_CD_AGREEMENT = 18
+REQUEST_MEDIATOR_DATA = 19
+
+REQUEST_TYPES = (
+    (REQUEST_SHIPMENTS, 'shipments'), (REQUEST_CITIES_ZONES, 'cities_zones'),
+    (REQUEST_CITIES, 'cities'), (REQUEST_CITIES_REGIONS, 'cities_regions'),
+    (REQUEST_CITIES_STREETS, 'cities_streets'),
+    (REQUEST_CITIES_QUARTERS, 'cities_quarters'), (REQUEST_OFFICES, 'offices'),
+    (REQUEST_POST_BOXES, 'post_boxes'),
+    (REQUEST_TARIFF_COURIER, 'tariff_courier'),
+    (REQUEST_TARIFF_POST, 'tariff_post'), (REQUEST_PROFILE, 'profile'),
+    (REQUEST_REGISTRATION_REQUEST, 'registration_request'),
+    (REQUEST_COUNTRIES, 'countries'),
+    (REQUEST_CANCEL_SHIPMENTS, 'cancel_shipments'),
+    (REQUEST_DELIVERY_DAYS, 'delivery_days'),
+    (REQUEST_ACCOUNT_ROLES, 'account_roles'),
+    (REQUEST_CLIENT_INFO, 'client_info'),
+    (REQUEST_ACCESS_CLIENTS, 'access_clients'),
+    (REQUEST_CHECK_CD_AGREEMENT, 'check_cd_agreement'),
+    (REQUEST_MEDIATOR_DATA, 'mediator_data')
+)
+
+TEMPLATE = "\
+<?xml version=\"1.0\" encoding=\"UTF-8\"?> \
+<request> \
+    <client> \
+        <username>{username}</username> \
+        <password>{password}</password> \
+    </client> \
+    <request_type>{request_type}</request_type> \
+    <updated_time>{updated_time}</updated_time> \
+    {data} \
+    <mediator>{mediator}</mediator> \
+    <client_software>{client_software}</client_software> \
+</request>"
+
+
+class Client(object):
+    def __init__(self, demo=False):
+        self.username = None
+        self.password = None
+        self.demo = demo
+
+    def login(self, username, password):
+        self.username = username
+        self.password = password
+        return self
+
+    def request(self,
+                request_type=None,
+                data=None,
+                mediator=None,
+                client_software="EcontPy"):
+
+        if not any(request_type in type_row for type_row in REQUEST_TYPES):
+            raise exceptions.InvalidRequestType
+
+        constructed_data = TEMPLATE.format(
+            username=self.username,
+            password=self.password,
+            request_type=self.get_request_type(request_type),
+            updated_time="2012-04-22 18:30:00",
+            data=data,
+            mediator=mediator,
+            client_software=client_software)
+
+        return http.Request(ECONT_DEMO_SERVICE_URL
+                                if self.demo else ECONT_SERVICE_URL).send(constructed_data)
+
+    def get_request_type(self, type_id=None):
+        return dict(REQUEST_TYPES)[type_id]
+
+    def get_shipments(self, shipments=[], full=False):
+        template = "<shipments full_tracking='{full}'>{data}</shipments>"
+        numbers = ""
+
+        for shipment in shipments:
+            numbers += "<num>{number}</num>".format(number=shipment)
+
+        data = template.format(full="ON" if full else "", data=numbers)
+
+        return self.request(REQUEST_SHIPMENTS, data)
