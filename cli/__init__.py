a9ce742918451fcb73ff3d916ddb828dc19999c4
Teodor Garzdin <tsg@hugelawn.com>
2016-10-18 12:36:10 +0200

    Error checking and better documentation.

--- cli/__init__.py
+++ cli/__init__.py
@@ -68,7 +68,17 @@ UPDATED_TIME_FORMAT = r"[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])\s(0[
 
 
 class Client(object):
-    def __init__(self, username, password, demo=False):
+    def __init__(self, username=None, password=None, demo=False):
+        """Instantiate the client object
+
+        Keyword args:
+        [string] username -- the econt.bg username for the account
+        [string] password -- the econt.bg password for the account
+        [bool] demo -- whether we're running in demo mode or not (default False)
+        """
+        if not username or not password:
+            raise Exception("No username or password supplied")
+            
         self.username = username
         self.password = password
         self.demo = demo
@@ -78,6 +88,14 @@ class Client(object):
                 data=None,
                 updated_time=None,
                 client_software="EcontPy"):
+        """Build and send a request with the given Parameters
+
+        Keyword args:
+        [int] request_type -- the type of the request from REQUEST_TYPES (default None)
+        [string] data -- the data to be sent with the request (default None)
+        [string] updated_time (optional) -- the time from which to return data (default None)
+        [string] client_software -- the name of the client, that is sending data (Default 'EcontPy')
+        """
 
         if not any(request_type in type_row for type_row in REQUEST_TYPES):
             raise Exception("Invalid request type")
@@ -98,14 +116,22 @@ class Client(object):
         return parse(response.text.encode('utf-8'))
 
     def get_request_type(self, type_id=None):
+        """Get the request type from REQUEST_TYPES by id
+
+        Keyword args:
+        [int] type_id -- the type id from REQUEST_TYPES (default None)
+        """
+        if not type_id:
+            raise Exception("No type id supplied")
+
         return dict(REQUEST_TYPES)[type_id]
 
     def get_shipments(self, shipments=None, detailed=False):
-        """
-        Get shipments with the supplied tracking id(s)
-        # Parameters
-            * shipments [int/string/array] - array of shipments id(s)
-            * full [bool] - whether to display information about the movement of the package
+        """Get shipments with the supplied tracking id(s)
+
+        Keyword args:
+        [int/string/array] shipments -- array of shipments id(s) (default None)
+        [bool] detailed - whether to display information about the movement of the package (default False)
         """
         if not shipments:
             raise Exception("Invalid shipment id(s)")
@@ -123,23 +149,26 @@ class Client(object):
 
         return self.request(REQUEST_SHIPMENTS, data)
 
-    def validate_address(self, city, post_code, quarter, street, street_num,
-                     building, entrance, floor, apartment, other, email):
+    def validate_address(self, city=None, post_code=None, quarter=None, street=None, street_num=None,
+                     building=None, entrance=None, floor=None, apartment=None, other=None, email=None):
+        """Check if an address is valid
+
+        Keyword args:
+        [string] city -- the name of the city (default None)
+        [int/string] post_code (optional) -- the post code of the city (default None)
+        [string] quarter (optional) -- the name of the quarter (default None)
+        [string] street (optional) -- the name of the street (default None)
+        [int/string] street_num (optional) -- the number of the street (default None)
+        [string] building (optional) -- the name of the building (default None)
+        [string] entrance (optional) -- the entrance name for a building (default None)
+        [int/string] floor (optional) -- the floor in the building (default None)
+        [int/string] apartment (optional) -- the number of the apartment (default None)
+        [string] other (optional) -- other data
+        [string] email (optional) -- an email to which to send information about a shipment on this address
         """
-        Check if an address is valid
-        # Parameters
-            * city [string] - The name of the city
-            * post_code [int/string] - The post code of the city
-            * quarter [string] - The name of the quarter
-            * street [string] - The name of the street
-            * street_num [int/string] - The number of the street
-            * building [string] - The name of the building
-            * entrance [string] - The entrance name for a building
-            * floor [int/string] - The floor in the building
-            * apartment [int/string] - The number of the apartment
-            * other [string] - Other data
-            * email [string] - An email to which to send information about a shipment on this address
-        """
+        if not city:
+            raise Exception("Invalid city supplied")
+
         template = "<address>\
                         <city>{city}</city>\
                         <post_code>{post_code}</post_code>\
@@ -170,10 +199,10 @@ class Client(object):
 
 
     def get_cities_zones(self, updated_time=None):
-        """
-        Get zone information within cities
-        # Parameters
-            * updated_time [string] (optional) - The date from which to display entries
+        """Get zone information within cities
+
+        Keyword args:
+        [string] updated_time (optional) -- the date from which to display entries (default None)
         """
         if updated_time:
             if not match(UPDATED_TIME_FORMAT, updated_time):
@@ -184,14 +213,14 @@ class Client(object):
         return self.request(REQUEST_CITIES_ZONES)
 
     def get_cities(self, cities=None, zone_id="all", report_type=None, updated_time=None):
-        """
-        Get cities (if at least one city is supplied it will do a search,
+        """Get cities (if at least one city is supplied it will do a search,
         otherwise it would return all cities)
-        # Parameters
-            * cities [array] - array of city names in cyrillic
-            * zone_id [int/string] - a zone to constrain the search (See get_cities_zones())
-            * report_type [string] - whether to display full information for a city or pass in 'short' for a shorter version
-            * updated_time [string] (optional) - The date from which to display entries
+
+        Keyword args:
+        [array] cities -- array of city names in cyrillic (default None)
+        [int/string] zone_id -- a zone to constrain the search to (default 'all')
+        [string] report_type -- whether to display full information for a city or pass in 'short' for a shorter version (default None)
+        [string] updated_time (optional) -- the date from which to display entries (default None)
         """
         if cities:
             city_names = []
@@ -219,10 +248,9 @@ class Client(object):
         return self.request(REQUEST_CITIES)
 
     def get_cities_regions(self, updated_time=None):
-        """
-        Get region information within cities
-        # Parameters
-            * updated_time [string] (optional) - The date from which to display entries
+        """Get region information within cities
+        Keyword args:
+        [string] updated_time (optional) -- the date from which to display entries (default None)
         """
         if updated_time:
             if not match(UPDATED_TIME_FORMAT, updated_time):
@@ -233,10 +261,10 @@ class Client(object):
         return self.request(REQUEST_CITIES_REGIONS)
 
     def get_cities_streets(self, updated_time=None):
-        """
-        Get street information within cities
-        # Parameters
-            * updated_time [string] (optional) - The date from which to display entries
+        """Get street information within cities
+
+        Keyword args:
+        [string] updated_time (optional) -- the date from which to display entries (default None)
         """
         if updated_time:
             if not match(UPDATED_TIME_FORMAT, updated_time):
@@ -247,10 +275,10 @@ class Client(object):
         return self.request(REQUEST_CITIES_STREETS)
 
     def get_cities_quarters(self, updated_time=None):
-        """
-        Get quarter information within cities
-        # Parameters
-            * updated_time [string] (optional) - The date from which to display entries
+        """Get quarter information within cities
+
+        Keyword args:
+        [string] updated_time (optional) -- the date from which to display entries (default None)
         """
         if updated_time:
             if not match(UPDATED_TIME_FORMAT, updated_time):
@@ -261,10 +289,10 @@ class Client(object):
         return self.request(REQUEST_CITIES_QUARTERS)
 
     def get_offices(self, updated_time=None):
-        """
-        Get offices information
-        # Parameters
-            * updated_time [string] (optional) - The date from which to display entries
+        """Get offices information
+
+        Keyword args:
+        [string] updated_time (optional) -- the date from which to display entries (default None)
         """
         if updated_time:
             if not match(UPDATED_TIME_FORMAT, updated_time):
@@ -275,10 +303,10 @@ class Client(object):
         return self.request(REQUEST_OFFICES)
 
     def get_post_boxes(self, locations=None):
-        """
-        Get post boxes information
-        # Parameters
-            * locations [array] - array of tuples in the form of (city_name, quarter_name)
+        """Get post boxes information
+
+        Keyword args:
+        [array] locations (optional) -- array of tuples in the form of (city_name, quarter_name)
         """
         if locations:
             locations_data = []
@@ -294,26 +322,22 @@ class Client(object):
         return self.request(REQUEST_POST_BOXES)
 
     def get_tariff_courier(self):
-        """
-        Get courier tariff information
-        """
+        """Get courier tariff information"""
         return self.request(REQUEST_TARIFF_COURIER)
 
     def get_tariff_post(self):
-        """
-        Get post tariff information
-        """
+        """Get post tariff information"""
         return self.request(REQUEST_TARIFF_POST)
 
     def get_profile(self):
-        """
-        Get profile information
-        """
+        """Get profile information"""
         return self.request(REQUEST_PROFILE)
 
     def get_registraion_request(self, email=None):
-        """
-        Get registration requests information
+        """Get registration requests information
+
+        Keyword args:
+        [string] email (optional) -- the email for which to get information about the request
         """
         if email:
             template = "<registration_request_mail>{data}</registration_request_mail>"
@@ -324,16 +348,14 @@ class Client(object):
         return self.request(REQUEST_REGISTRATION_REQUEST)
 
     def get_countries(self):
-        """
-        Get countries information
-        """
+        """Get countries information"""
         return self.request(REQUEST_COUNTRIES)
 
     def cancel_shipments(self, shipments=None):
-        """
-        Cancel shipments with the supplied ids
-        # Parameters
-            * shipments [array] - array of shipments ids
+        """Cancel shipments with the supplied ids
+
+        Keyword args:
+        [array] shipments -- array of shipments ids
         """
         if not shipments:
             raise Exception("Invalid shipment id(s) supplied")
@@ -349,8 +371,10 @@ class Client(object):
         return self.request(REQUEST_CANCEL_SHIPMENTS, data)
 
     def get_delivery_days(self, date=None):
-        """
-        Get available shipping days based on the current date
+        """Get available shipping days based on a given date
+
+        Keyword args:
+        [string] date -- the date from which to check (default None)
         """
         if not date:
             raise Exception("Invalid date supplied")
@@ -364,12 +388,12 @@ class Client(object):
         return self.request(REQUEST_DELIVERY_DAYS, data)
 
     def get_client_info(self, ein=None, egn=None, client_id=None):
-        """
-        Get information about a client
-        # Parameters
-            * ein [int/string] - the EIN of the client
-            * egn [int/string] - The EGN of the client
-            * id [int/string] - The ID of the client
+        """Get information about a client
+
+        Keyword args:
+        [int/string] ein - the EIN of the client
+        [int/string] egn - the EGN of the client
+        [int/string] id - the ID of the client
         """
         if not ein or not egn or not client_id:
             raise Exception("Invalid client information")
@@ -380,17 +404,15 @@ class Client(object):
         return self.request(REQUEST_CLIENT_INFO, data)
 
     def get_clients(self):
-        """
-        Get information about the clients of the current user
-        """
+        """Get information about the clients of the current user"""
         return self.request(REQUEST_ACCESS_CLIENTS)
 
     def check_cd_agreement(self, client_name=None, cd_agreement_id=None):
-        """
-        Verify the cash on delivery information
-        # Parameters
-            * client_name [string] - The name of the client
-            * cd_agreement_id [int/string] - The id of the cash delivery agreement
+        """Verify the cash on delivery information
+
+        Keyword args:
+        [string] client_name - the name of the client (default None)
+        [int/string] cd_agreement_id - the id of the cash delivery agreement (default None)
         """
         if not client_name or not cd_agreement_id:
             raise Exception("Invalid Cash on Delivery information")
@@ -401,11 +423,11 @@ class Client(object):
         return self.request(REQUEST_CHECK_CD_AGREEMENT, data)
 
     def get_mediator_data(self, mediator_id=None, from_date=None):
-        """
-        Get mediator information
-        # Parameters
-            * mediator_id [int/string] - The id of the mediator
-            * from_date [string] (optional) - The date agains which to check
+        """Get mediator information
+
+        Keyword args:
+        [int/string] mediator_id - the id of the mediator (default None)
+        [string] from_date (optional) - the date agains which to check (default None)
         """
         if not mediator_id:
             raise Exception("No mediator id supplied")
@@ -424,4 +446,4 @@ class Client(object):
 
 if __name__ == '__main__':
     c = Client("iasp-dev", "iasp-dev", True)
-    c.get_shipments([12345,123])
+    print(c.validate_address("Плевен", 5800, "Център", "Илия Бешков", 19, "", "", 5, 14, "", ""))
